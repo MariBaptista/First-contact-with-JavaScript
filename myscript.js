fade0a8324432a5bac8143f1605a10af4c1fdc39
Marina Baptista <100391260+MariBaptista@users.noreply.github.com>
2022-12-09 22:27:24 -0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function Acender(){
+    document.getElementById('lampada').src='imagens/pic_bulbon.gif'
+
+}
+
+function Apagar(){
+    document.getElementById('lampada').src='imagens/pic_bulboff.gif'
+
+}
